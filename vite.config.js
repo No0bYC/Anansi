379e53aab39d5f650778b4c9c0619ba0d75753a8
No0bYC <63961392+No0bYC@users.noreply.github.com>
2026-06-28 11:38:29 +0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    target: 'esnext'
+  },
+  esbuild: {
+    supported: {
+      'top-level-await': true
+    }
+  }
 })
